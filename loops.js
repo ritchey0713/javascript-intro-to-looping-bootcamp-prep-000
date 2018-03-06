722618f460f80276fb19904bed46d345748fa958
david ritchey <ritchey0713@gmail.com>
2018-03-06 03:56:53 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -13,10 +13,10 @@ wrapGift(gifts[2])
 function forLoop (array){ 
 for (let i = 0; i < 25; i++ ){
 if (i===1){
-  console.log("I am 1 strange loop.");
+  array.push("I am 1 strange loop.");
 }
 else{
-  console.log("I am ${i} strange loops");
+     ("I am ${i} strange loops");
 }    
   }
 return array;
