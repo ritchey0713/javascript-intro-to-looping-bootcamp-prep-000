11bc6ec0749278abc9ad72455c7091ac09453c12
david ritchey <ritchey0713@gmail.com>
2018-03-06 04:09:10 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -34,14 +34,10 @@ function doWhile (array){
   function maybeTrue() {
   return Math.random() >= 0.5
 }
-
-  do{array.pop()
-    
-  }
+ do{array.pop()
+      }
 while(array.length>0 && maybeTrue());
 return array
-
-  
 }
 
 
