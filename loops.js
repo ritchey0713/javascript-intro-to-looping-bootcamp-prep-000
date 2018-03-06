c24eff04713755e895c676d051ddb7ce1845eed0
david ritchey <ritchey0713@gmail.com>
2018-03-06 03:29:38 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,20 @@
+/*
+var gifts = ["teddy bear", 'drone', "doll"]
+
+function wrapGift (gift){
+  console.log("wrapped ${gift} and added a bow!")
+}
+
+wrapGift(gifts[0])
+wrapGift(gifts[1])
+wrapGift(gifts[2])
+*/
+
+for (var i =0; i < 25; i++ ){
+if (i===1){
+  console.log("I am 1 strange loop.");
+}
+else{
+  
+}    
+  }
